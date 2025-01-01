b8204337d45ea3e8cce0ebdc8cbfffc3c364e289
yuri <arc.bunny.121@gmail.com>
2025-01-02 00:52:29 +0900

    ダミーデータ

---
+++ src/database/seeders/SeasonsTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class SeasonsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+
+        $param = [
+            'name' => '春',
+        ];
+        DB::table('seasons')->insert($param);
+
+        $param = [
+            'name' => '夏',
+        ];
+        DB::table('seasons')->insert($param);
+
+        $param = [
+            'name' => '秋',
+        ];
+        DB::table('seasons')->insert($param);
+
+        $param = [
+            'name' => '冬',
+        ];
+        DB::table('seasons')->insert($param);
+    }
+}
