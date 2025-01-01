d9b03c88a6332b2363461f17dc847cf75ea171ca
yuri <arc.bunny.121@gmail.com>
2025-01-01 19:57:24 +0900

    view,css　追加編集

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rese</title>
+    <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @yield('css')
+</head>
+
+<body>
+    <header class="header__l">
+        
+        @yield('header')
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+
+</body>
+
+</html>
